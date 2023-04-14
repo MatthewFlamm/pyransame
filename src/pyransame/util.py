54c76b0fdc89965c6af584b2a0ccbde27d23b5a2
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-04-14 15:45:02 +0000

    unneeded util

--- src/pyransame/util.py
+++ src/pyransame/util.py
@@ -16,14 +16,3 @@ def _generate_points_in_tri(a: np.ndarray, b: np.ndarray, c: np.ndarray, n: int=
             r1, r2 = np.random.random(), np.random.random()
         points[i, :] = a + r1 * v1 + r2 * v2
     return points
-
-def _is_point_inside_triangle(v1: np.ndarray, v2: np.ndarray, p: np.ndarray) -> bool:
-
-    denom = np.cross(v1, v2)
-
-    a = np.cross(p, v1) / denom
-    b = np.cross(p, v2) / denom
-
-    if a < 0 or b < 0 or a + b > 1:
-        return False
-    return True
