d8239472c1041b5b4a384a4a16fe9fa7add6e112
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-04-17 18:41:04 +0000

    reflect points inside triangle

--- src/pyransame/util.py
+++ src/pyransame/util.py
@@ -14,8 +14,9 @@ def _generate_points_in_tri(
     points = np.empty((n, 3), dtype=float)
 
     for i in range(n):
-        r1, r2 = 100.0, 100.0
-        while r1 + r2 > 1.0:
-            r1, r2 = rng.random(), rng.random()
+        r1, r2 = rng.random(), rng.random()
+        if r1 + r2 > 1.0:
+            r1 = 1 - r1
+            r2 = 1 - r2
         points[i, :] = a + r1 * v1 + r2 * v2
     return points
