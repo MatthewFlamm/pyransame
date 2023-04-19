01eacfbb11e1385bd1990386a2fc1950b47dee6a
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-04-19 18:03:43 +0000

    better tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -6,7 +6,7 @@ from hypothesis import assume, given
 from hypothesis import strategies as st
 from hypothesis.extra.numpy import arrays
 
-from pyransame.util import _generate_points_in_tri, _generate_points_in_tetra
+from pyransame.util import _generate_points_in_tetra, _generate_points_in_tri
 
 
 # Use min_value and max_value to avoid numerical imprecision artifacts, but still span a large space
@@ -18,16 +18,10 @@ from pyransame.util import _generate_points_in_tri, _generate_points_in_tetra
 def test_generate_points_in_tri(a, b, c):
     """Use pyvista builtin find_containing_cell to test."""
     # make sure that the points are not coincident
-    assume(not np.allclose(a, b, rtol=1e-3, atol=1e-3))
-    assume(not np.allclose(b, c, rtol=1e-3, atol=1e-3))
-    assume(not np.allclose(a, c, rtol=1e-3, atol=1e-3))
-    # make sure that the two vectors are not coincident
-    assume(np.linalg.norm(np.cross(b - a, c - a)) > 1e-4)
+    tri = pv.Triangle([a, b, c])
+    assume(tri.area > 1e-4)
 
     points = _generate_points_in_tri(a, b, c, 1000)
-
-    tri = pv.Triangle([a, b, c])
-
     for i in range(1000):
         assert tri.find_containing_cell(points[i, :]) == 0
 
@@ -49,7 +43,7 @@ def test_uniformity_tri():
     exp_distance = (
         1 / 72 * (8 * np.sqrt(3) + 3 * np.arcsinh(np.sqrt(3)) + np.log(2 + np.sqrt(3)))
     )
-    assert distances.mean() == pytest.approx(exp_distance, rel=1e-3)
+    assert distances.mean() == pytest.approx(exp_distance, rel=2e-3)
 
     distances = np.linalg.norm(points, axis=-1)
     assert distances.mean() == pytest.approx(1 / 12 * (4 + 3 * np.log(3)), rel=2e-3)
@@ -59,10 +53,10 @@ def test_uniformity_tri():
 # Weisstein, Eric W. "Triangle Point Picking." From MathWorld--A Wolfram Web Resource.
 def test_uniformity_tetra():
     # form regular tetrahedron with geometric center at origin
-    a = np.array((np.sqrt(3)/3.0, 0.0, -np.sqrt(6)/12.0))
-    b = np.array((-np.sqrt(3)/6, 0.5, -np.sqrt(6)/12.0))
-    c = np.array((-np.sqrt(3)/6, -0.5, -np.sqrt(6)/12.0))
-    d = np.array((0.0, 0.0, np.sqrt(6)/4))
+    a = np.array((np.sqrt(3) / 3.0, 0.0, -np.sqrt(6) / 12.0))
+    b = np.array((-np.sqrt(3) / 6, 0.5, -np.sqrt(6) / 12.0))
+    c = np.array((-np.sqrt(3) / 6, -0.5, -np.sqrt(6) / 12.0))
+    d = np.array((0.0, 0.0, np.sqrt(6) / 4))
 
     center = np.array((0.0, 0.0, 0.0))
 
@@ -70,3 +64,23 @@ def test_uniformity_tetra():
     points = _generate_points_in_tetra(a, b, c, d, 2000000)
 
     assert np.allclose(points.mean(axis=0), center, rtol=1e-3, atol=1e-3)
+
+
+# Use min_value and max_value to avoid numerical imprecision artifacts, but still span a large space
+@given(
+    arrays(float, 3, elements=st.floats(min_value=-1000.0, max_value=1000.0)),
+    arrays(float, 3, elements=st.floats(min_value=-1000.0, max_value=1000.0)),
+    arrays(float, 3, elements=st.floats(min_value=-1000.0, max_value=1000.0)),
+    arrays(float, 3, elements=st.floats(min_value=-1000.0, max_value=1000.0)),
+)
+def test_generate_points_in_tetra(a, b, c, d):
+    """Use pyvista builtin find_containing_cell to test."""
+    # make sure that the points are not coincident
+    cells = [4, 0, 1, 2, 3]
+    celltypes = [pv.CellType.TETRA]
+    tetra = pv.UnstructuredGrid(cells, celltypes, [a, b, c, d])
+    assume(tetra.volume > 1e-4)
+
+    points = _generate_points_in_tetra(a, b, c, d, 1000)
+    for i in range(1000):
+        assert tetra.find_containing_cell(points[i, :]) == 0
