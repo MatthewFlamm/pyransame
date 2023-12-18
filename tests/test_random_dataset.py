a1dae7c4e9f2d526516895c9693b7cb1ac845824
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-12-18 20:25:18 +0000

    add test for random_line_dataset

--- tests/test_random_dataset.py
+++ tests/test_random_dataset.py
@@ -27,3 +27,15 @@ def test_random_volume_dataset():
     assert np.allclose(sampled.points[:, 0], sampled["x"])
     assert np.allclose(sampled.points[:, 1], sampled["y"])
     assert np.allclose(sampled.points[:, 2], sampled["z"])
+
+
+def test_random_line_dataset():
+    mesh = pv.Line()
+    mesh["x"] = mesh.points[:, 0]
+    mesh["y"] = mesh.points[:, 1]
+    mesh["z"] = mesh.points[:, 2]
+
+    sampled = pyransame.random_line_dataset(mesh, 50)
+    assert np.allclose(sampled.points[:, 0], sampled["x"])
+    assert np.allclose(sampled.points[:, 1], sampled["y"])
+    assert np.allclose(sampled.points[:, 2], sampled["z"])
