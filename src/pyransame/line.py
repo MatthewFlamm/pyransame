721025ada7384c6c42c6e9f45012e155394bfb97
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-12-18 19:50:15 +0000

    add random_line_points and random_line_dataset.  tests for random_line_points

---
+++ src/pyransame/line.py
@@ -0,0 +1,159 @@
+"""Generating random points on a 2D surface."""
+from typing import Optional, Union
+
+import numpy as np
+import numpy.typing as npt
+import pyvista as pv
+
+import pyransame
+import pyransame.util as util
+
+
+def random_line_points(
+    mesh: pv.DataSet,
+    n: int = 1,
+    weights: Optional[Union[str, npt.ArrayLike]] = None,
+) -> np.ndarray:
+    """
+    Generate random points on lines.
+
+    Supported cell types:
+
+    - Line
+
+    Parameters
+    ----------
+    mesh : pyvista.DataSet
+        The mesh for which to generate random points.  Must have cells.
+
+    n : int, default: 1
+        Number of random points to generate.
+
+    weights : str, or array_like, optional
+        Weights to use for probability of choosing points inside each cell.
+
+        If a ``str`` is supplied, it will use the existing cell data on ``mesh``.
+
+    Returns
+    -------
+    points : np.ndarray
+        ``(n, 3)`` points that exist inside cells on ``mesh``.
+
+    Examples
+    --------
+    >>> import pyransame
+    >>> import pyvista as pv
+    >>> p = [
+    ...       [0., 0., 0.],
+    ...       [1., 0., 0.],
+    ...       [1., 2., 0.]
+    ... ]
+    >>> mesh = pv.PolyData(p, lines=[2, 0, 1, 2, 1, 2]
+    >>> points = pyransame.random_surface_points(mesh, n=5)
+
+    Now plot result.
+
+    >>> pl = pv.Plotter()
+    >>> _ = pl.add_mesh(mesh, color='tan')
+    >>> _ = pl.add_points(points, render_points_as_spheres=True, point_size=10.0, color='red')
+    >>> pl.view_xy()
+    >>> pl.show(cpos=cpos)
+    """
+    if weights is None:
+        weights = np.ones(mesh.n_cells)
+
+    if isinstance(weights, str):
+        weights = mesh.cell_data[weights]
+
+    weights = np.asanyarray(weights)
+
+    if n <= 0:
+        raise ValueError(f"n must be > 0, got {n}")
+
+    n_cells = mesh.n_cells
+
+    if "Length" not in mesh.cell_data:
+        mesh = mesh.compute_cell_sizes(length=True, area=False, volume=False)  # type: ignore
+
+    p = weights * mesh["Length"]
+
+    if p.sum() == 0:
+        raise ValueError("No cells with length in DataSet")
+
+    p = p / p.sum()
+
+    chosen_cells, unique_counts, point_indices = util._random_cells(n_cells, n, p)
+    points = np.empty((n, 3))
+    for i, (chosen_cell, count) in enumerate(zip(chosen_cells, unique_counts)):
+        c = mesh.get_cell(chosen_cell)
+        if c.type == pv.CellType.LINE:
+            points[
+                point_indices[i] : point_indices[i + 1], :
+            ] = util._generate_points_in_line(c.points, count)
+        else:
+            raise NotImplementedError(
+                f"Random generation for {c.type.name} not yet supported"
+            )
+    return points
+
+
+def random_line_dataset(
+    mesh: pv.DataSet,
+    n: int = 1,
+    weights: Optional[Union[str, npt.ArrayLike]] = None,
+) -> pv.PolyData:
+    """
+    Generate random points on surface with sampled data.
+
+    Supported cell types:
+
+    - Triangle
+    - Triangle Strip
+    - Pixel
+    - Polygon
+    - Quad
+
+    All cells must be convex.
+
+    Parameters
+    ----------
+    mesh : pyvista.DataSet
+        The mesh for which to generate random points.  Must have cells.
+
+    n : int, default: 1
+        Number of random points to generate.
+
+    weights : str, or array_like, optional
+        Weights to use for probability of choosing points inside each cell.
+
+        If a ``str`` is supplied, it will use the existing cell data on ``mesh``.
+
+    Returns
+    -------
+    points : pv.PolyData
+        ``(n, 3)`` points that exist inside cells on ``mesh`` and with
+        sampled data.
+
+    Examples
+    --------
+    >>> import pyransame
+    >>> import pyvista as pv
+    >>> from pyvista import examples
+    >>> mesh = examples.download_bunny()
+    >>> mesh['y'] = mesh.points[:, 1]
+    >>> points = pyransame.random_surface_dataset(mesh, n=500)
+
+    Now plot result.
+
+    >>> cpos = [
+    ...     (-0.07, 0.2, 0.5),
+    ...     (-0.02, 0.1, -0.0),
+    ...     (0.04, 1.0, -0.2),
+    ... ]
+    >>> pl = pv.Plotter()
+    >>> _ = pl.add_mesh(mesh, color='tan')
+    >>> _ = pl.add_points(points, scalars='y', render_points_as_spheres=True, point_size=10.0)
+    >>> pl.show(cpos=cpos)
+    """
+    points = random_line_points(mesh, n, weights)
+    return pv.PolyData(points).sample(mesh, locator="static_cell")
---
+++ tests/test_random_line_points.py
@@ -0,0 +1,102 @@
+"""Tests for random_surface_points."""
+import numpy as np
+import pytest
+import pyvista as pv
+
+import pyransame
+
+
+@pytest.fixture
+def line():
+    return pv.PolyData(
+        [[-1.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0.0]], lines=[2, 0, 1, 2, 1, 2]
+    )
+
+
+@pytest.fixture
+def nonuniform_line():
+    points = np.array([[-2.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0]])
+    return pv.PolyData(points, lines=[2, 0, 1, 2, 1, 2])
+
+
+def test_cell_types(line):
+    assert line.get_cell(0).type == pv.CellType.LINE
+    pyransame.random_line_points(line, 20)
+
+
+def test_mixed_types():
+    # as long as there are 1D cells, we should be able to sample even if there are other cell types
+    # adds a 1D LINE to an unstructured grid with VOXEL cells
+    uniform_mesh = pv.ImageData(dimensions=(4, 4, 4)).cast_to_unstructured_grid()
+    points = uniform_mesh.points.copy()
+    cells = uniform_mesh.cells.copy()
+    cell_types = uniform_mesh.celltypes.copy()
+    cells = np.append(cells, [2, 0, 1])
+    cell_types = np.append(cell_types, pv.CellType.LINE)
+    mesh = pv.UnstructuredGrid(cells, cell_types, uniform_mesh.points)
+    pyransame.random_line_points(mesh, 20)
+
+
+def test_unsupported_types():
+    mesh = pv.ImageData(dimensions=(4, 4, 4))
+    assert all([c.type == pv.CellType.VOXEL for c in mesh.cell])
+    with pytest.raises(ValueError, match="No cells with length in DataSet"):
+        pyransame.random_line_points(mesh, 20)
+
+
+def test_straight_line(line):
+    points = pyransame.random_line_points(line, 200000)
+    assert points.shape == (200000, 3)
+    assert isinstance(points, np.ndarray)
+    assert np.allclose(points.mean(axis=0), (0.0, 0.0, 0.0), rtol=5e-3, atol=5e-3)
+
+
+def test_small_sample(line):
+    points = pyransame.random_line_points(line, 2)
+    assert points.shape == (2, 3)
+    assert isinstance(points, np.ndarray)
+
+
+def test_nonuniform_cell_size(nonuniform_line):
+    points = pyransame.random_line_points(nonuniform_line, 200000)
+    assert np.allclose(points.mean(axis=0), (-0.5, 0.0, 0.0), rtol=5e-3, atol=5e-3)
+
+
+def test_nonuniform_cell_size_w_precomputed_areas(nonuniform_line):
+    mesh = nonuniform_line.compute_cell_sizes(length=False, volume=False)
+
+    points = pyransame.random_line_points(mesh, 200000)
+    assert np.allclose(points.mean(axis=0), (-0.5, 0.0, 0.0), rtol=5e-3, atol=5e-3)
+
+
+def test_weights(nonuniform_line):
+    # 1/2 the number of points will have y>0 if no weighting is done
+    # The expected y_mean will be -1 for points in cell 0 and 0.5 for piotns in cell 1
+    # Therefore a weighting of 1:4 must be applied to have center be at y=0.0
+
+    nonuniform_line.cell_data["weights"] = [1, 4]
+
+    points = pyransame.random_line_points(nonuniform_line, 200000, "weights")
+    assert np.allclose(points.mean(axis=0), (0.0, 0.0, 0.0), rtol=5e-3, atol=5e-3)
+
+    nonuniform_line.cell_data.clear()
+    nonuniform_line.cell_data["other_str"] = [1, 4]
+
+    points = pyransame.random_line_points(nonuniform_line, 200, "other_str")
+
+
+def test_weights_array(nonuniform_line):
+    points = pyransame.random_line_points(nonuniform_line, 200000, [1, 4])
+    assert np.allclose(points.mean(axis=0), (0.0, 0.0, 0.0), rtol=5e-3, atol=5e-3)
+
+
+def test_wrong_weights(line):
+    weights = {"not a good entry": "should raise an error"}
+
+    with pytest.raises(TypeError):
+        pyransame.random_surface_points(line, 20, weights=weights)
+
+
+def test_wrong_n(line):
+    with pytest.raises(ValueError, match="n must be > 0, got -20"):
+        pyransame.random_surface_points(line, -20)
