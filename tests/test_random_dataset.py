4e83f09c0fa776b5d5802eae1ae28590603370dc
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2024-07-12 23:57:26 +0000

    add coverage for dataset method

--- tests/test_random_dataset.py
+++ tests/test_random_dataset.py
@@ -40,3 +40,16 @@ def test_random_line_dataset():
     assert np.allclose(sampled.points[:, 0], sampled["x"])
     assert np.allclose(sampled.points[:, 1], sampled["y"])
     assert np.allclose(sampled.points[:, 2], sampled["z"])
+
+
+def test_random_vertex_dataset():
+    mesh = pv.ImageData(dimensions=(10, 10, 10))
+    mesh = pv.PolyData(mesh.points)  # make all vertex cells
+    mesh.point_data["x"] = mesh.points[:, 0]
+    mesh.point_data["y"] = mesh.points[:, 1]
+    mesh.point_data["z"] = mesh.points[:, 2]
+
+    sampled = pyransame.random_vertex_dataset(mesh, 50)
+    assert np.allclose(sampled.points[:, 0], sampled["x"])
+    assert np.allclose(sampled.points[:, 1], sampled["y"])
+    assert np.allclose(sampled.points[:, 2], sampled["z"])
